8465f4e5eb1e9148cc88f0c42950b2cde7426d7e
Denis Epifanov <36046200+sequencyyy@users.noreply.github.com>
2025-06-22 02:02:32 +0500

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
-DATABASE_URL = "postgresql://postgres:postgres@db:5432/audio"
+DATABASE_URL = "postgresql://postgres:*@db:5432/audio"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
