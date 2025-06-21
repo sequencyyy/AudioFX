96d73c843f4feaed0ad09160a1d3eb5073b3e681
Denis Epifanov <36046200+sequencyyy@users.noreply.github.com>
2025-06-22 02:03:22 +0500

    Update tasks.py

--- backend/tasks.py
+++ backend/tasks.py
@@ -10,7 +10,7 @@ from models import ProcessingHistory
 from db import SessionLocal
 
 
-DATABASE_URL = "postgresql://postgres:postgres@db:5432/audio"
+DATABASE_URL = "postgresql://postgres:*@db:5432/audio"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
